faefd9760ca9d2fd5aa251ad5eee10fd7412445c
aekdahl <alexander.ekdahl@gmail.com>
2024-12-12 18:06:53 +0100

    Create docusaurus.config.js

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'LangSwarm Docs',
+  tagline: 'Documentation for LangSwarm',
+  url: 'https://aekdahl.github.io',
+  baseUrl: '/LangSwarm/',
+  organizationName: 'aekdahl',
+  projectName: 'LangSwarm',
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+        },
+      },
+    ],
+  ],
+};
