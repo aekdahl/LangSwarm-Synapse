a8372db92c1ec4aed8134c412bcbca7e1b7c5431
aekdahl <alexander.ekdahl@gmail.com>
2024-12-20 14:07:22 +0100

    Update swarm.py

--- langswarm/swarm/swarm.py
+++ langswarm/swarm/swarm.py
@@ -54,7 +54,8 @@ class Swarm:
         requirements=None,
         paraphrase_threshold=0.8,
         model='all-MiniLM-L6-v2',
-        instructions='You are a helpful assistant.'
+        instructions='You are a helpful assistant.',
+        logger=None
     ):
         self.llms = llms or []
         self.query = query
@@ -83,16 +84,19 @@ class Swarm:
 
         if self.verbose and not self.clients:
             print("\nBots:", self.bots)
-        
-        self.logger = logging.getLogger("LangSwarm.Core")
-        if not self.logger.hasHandlers():
-            handler = logging.StreamHandler()
-            formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-            handler.setFormatter(formatter)
-            self.logger.addHandler(handler)
-            self.logger.setLevel(logging.INFO)
 
-        self.logger.info("Core logger initialized.")
+        if logger:
+            self.logger = logger
+        else:
+            self.logger = logging.getLogger("LangSwarm.Core")
+            if not self.logger.hasHandlers():
+                handler = logging.StreamHandler()
+                formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+                handler.setFormatter(formatter)
+                self.logger.addHandler(handler)
+                self.logger.setLevel(logging.INFO)
+    
+            self.logger.info("Core logger initialized.")
 
     def check_initialization(self):
         """
