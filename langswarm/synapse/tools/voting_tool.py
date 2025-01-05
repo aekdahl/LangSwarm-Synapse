ce14f187c13214463bf7a67837d6ed68232e2860
aekdahl <alexander.ekdahl@gmail.com>
2025-01-05 12:55:20 +0100

    Update voting_tool.py

--- langswarm/synapse/tools/voting_tool.py
+++ langswarm/synapse/tools/voting_tool.py
@@ -11,6 +11,12 @@ from langchain.tools import Tool
 from langswarm.synapse.swarm.voting import LLMVoting
 
 class LangSwarmVotingTool(Tool):
+    class Config:
+        """
+        Configure the tool to allow extra fields.
+        """
+        extra = "allow"  # Permit extra attributes
+        
     def __init__(self, agents, **kwargs):
         """
         Initializes the LangSwarmVotingTool.
@@ -19,23 +25,12 @@ class LangSwarmVotingTool(Tool):
         - agents (list): List of agents to use in the voting process.
         - kwargs: Additional parameters for the LLMVoting class.
         """
+        self.voting = LLMVoting(clients=agents, **kwargs)
         super().__init__(
             name="LangSwarm Voting",
             func=self.run,
             description="A tool to enable voting-based decision-making among agents."
         )
-        self._agents = agents
-        self._kwargs = kwargs
-
-    @property
-    def voting(self):
-        """
-        Lazy-loads and returns an instance of LLMVoting.
-        """
-        if not hasattr(self, "_voting"):
-            # Instantiate LLMVoting only when accessed
-            self._voting = LLMVoting(clients=self._agents, **self._kwargs)
-        return self._voting
 
     def run(self, query):
         """
