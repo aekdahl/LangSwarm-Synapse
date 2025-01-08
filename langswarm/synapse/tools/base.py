875a1012edac591ae3f6727c558e0c88980819f1
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 17:37:10 +0100

    Update base.py

--- langswarm/synapse/tools/base.py
+++ langswarm/synapse/tools/base.py
@@ -4,4 +4,9 @@ class BaseTool:
         self.description = description
 
     def use(self, *args, **kwargs):
+        """Override this method to define the tool's behavior."""
         raise NotImplementedError("This method should be implemented in a subclass.")
+
+    def run(self, *args, **kwargs):
+        """Redirects to the `use` method for compatibility with LangChain tools."""
+        return self.use(*args, **kwargs)
